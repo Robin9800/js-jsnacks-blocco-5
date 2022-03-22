c8169b721506af62a4cefce0af37aa27b98dd41e
Robin9800 <robin.costanzo@gmail.com>
2022-03-22 16:24:12 +0100

    array di oggetti creato

---
+++ js-snack-1/script.js
@@ -0,0 +1,56 @@
+/*Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna
+varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine.*/
+
+//Creo un array di 10 oggetti che rappresentano una zucchina
+const zucchina = [
+    {
+        varietà: "nera",
+        peso: 20,
+        lunghezza: 19
+    },
+    {
+        varietà: "viola",
+        peso: 220,
+        lunghezza: 14
+    },
+    {
+        varietà: "albina",
+        peso: 30,
+        lunghezza: 16
+    },
+    {
+        varietà: "arancione",
+        peso: 10,
+        lunghezza: 11
+    },
+    {
+        varietà: "nera",
+        peso: 210,
+        lunghezza: 16
+    },
+    {
+        varietà: "nera",
+        peso: 20,
+        lunghezza: 14
+    },
+    {
+        varietà: "nera",
+        peso: 20,
+        lunghezza: 14
+    },
+    {
+        varietà: "nera",
+        peso: 20,
+        lunghezza: 14
+    },
+    {
+        varietà: "nera",
+        peso: 20,
+        lunghezza: 14
+    },
+    {
+        varietà: "nera",
+        peso: 20,
+        lunghezza: 14
+    },
+]
